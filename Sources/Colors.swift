eb1f2c99ddc6f31b1fc7db26eca0cace4ef2539a
Nick Platt <platt.nicholas@gmail.com>
2016-09-09 02:25:47 -0400

    Initial commit. This is all of it.

---
+++ Sources/Colors.swift
@@ -0,0 +1,33 @@
+import Cocoa
+
+public struct FlatUI {
+    public static let clouds = NSColor.init(red: 236, green: 240, blue: 241, alpha: 1)
+    public static let silver = NSColor.init(red: 189, green: 195, blue: 199, alpha: 1)
+    
+    public static let concrete = NSColor.init(red: 149, green: 165, blue: 166, alpha: 1)
+    public static let asbestos = NSColor.init(red: 127, green: 140, blue: 141, alpha: 1)
+    
+    public static let alizarin = NSColor.init(red: 231, green: 76, blue: 60, alpha: 1)
+    public static let pomegranate = NSColor.init(red: 192, green: 57, blue: 43, alpha: 1)
+    
+    public static let carrot = NSColor.init(red: 230, green: 126, blue: 34, alpha: 1)
+    public static let pumpkin = NSColor.init(red: 211, green: 84, blue: 0, alpha: 1)
+    
+    public static let sunFlower = NSColor.init(red: 241, green: 196, blue: 15, alpha: 1)
+    public static let orange = NSColor.init(red: 243, green: 156, blue: 18, alpha: 1)
+    
+    public static let emerald   = NSColor.init(red: 46, green: 204, blue: 113, alpha: 1)
+    public static let nephritis = NSColor.init(red: 39, green: 174, blue: 96, alpha: 1)
+    
+    public static let turquoise = NSColor.init(red: 26, green: 188, blue: 156, alpha: 1)
+    public static let greenSea = NSColor.init(red: 22, green: 160, blue: 133, alpha: 1)
+    
+    public static let peterRiver = NSColor.init(red: 52, green: 152, blue: 219, alpha: 1)
+    public static let belizeHole = NSColor.init(red: 41, green: 128, blue: 185, alpha: 1)
+    
+    public static let amethyst = NSColor.init(red: 155, green: 89, blue: 182, alpha: 1)
+    public static let wisteria = NSColor.init(red: 142, green: 68, blue: 173, alpha: 1)
+    
+    public static let wetAsphalt = NSColor.init(red: 52, green: 73, blue: 94, alpha: 1)
+    public static let midnightBlue = NSColor.init(red: 44, green: 62, blue: 80, alpha: 1)
+}
---
+++ Sources/FlatUIColors.h
@@ -0,0 +1,19 @@
+//
+//  FlatUIColors iOS.h
+//  FlatUIColors iOS
+//
+//  Created by Nicholas Platt on 9/9/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlatUIColors iOS.
+FOUNDATION_EXPORT double FlatUIColorsVersionNumber;
+
+//! Project version string for FlatUIColors iOS.
+FOUNDATION_EXPORT const unsigned char FlatUIColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatUIColors_iOS/PublicHeader.h>
+
+
