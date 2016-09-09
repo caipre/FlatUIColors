f350981f0f09db371d00074c18d4a47df5e376a8
Nick Platt <platt.nicholas@gmail.com>
2016-09-09 02:37:58 -0400

    Remove iOS artifacts

--- Sources/FlatUIColors.h
+++ Sources/FlatUIColors.h
@@ -1,19 +1,16 @@
 //
-//  FlatUIColors iOS.h
-//  FlatUIColors iOS
+//  FlatUIColors.h
 //
 //  Created by Nicholas Platt on 9/9/16.
 //
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for FlatUIColors iOS.
+//! Project version number for FlatUIColors.
 FOUNDATION_EXPORT double FlatUIColorsVersionNumber;
 
-//! Project version string for FlatUIColors iOS.
+//! Project version string for FlatUIColors.
 FOUNDATION_EXPORT const unsigned char FlatUIColorsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <FlatUIColors_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <FlatUIColors/PublicHeader.h>
 
 
